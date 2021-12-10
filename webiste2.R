88a52b6db8e5fb8c90e5cf9004b9b242ec3f746a
seredes <serena.destefani@rutgers.edu>
2021-12-10 18:04:03 -0500

    first commit

---
+++ webiste2.R
@@ -0,0 +1,7 @@
+library(blogdown)
+blogdown::new_site(theme = "radity/raditian-free-hugo-theme")
+
+new_site(theme = "hugo-apero/hugo-apero", 
+         format = "toml",
+         sample = FALSE,
+         empty_dirs = TRUE)
